c4b863fafc6cb04370c8364849b6eb5afbf225be
patel3ja <patel3ja@mail.uc.edu>
2021-01-30 17:17:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+
+    System.out.println("Enter a number to count down or up from: ");
+    
+    int num = s.nextInt();
+
+    if (num >= 0) 
+    {
+    while (num >= 0) 
+    {
+      System.out.println(num);
+      num--;
+    }
+    } else 
+    {
+      while (num <= 0)
+      {
+      System.out.println(num);
+      num++;
+      }
+    }
+    System.out.println("Blast Off!");
+    
+  }
+}
